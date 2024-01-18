2c778786618dede5562395f1be35530cd6c4fcab
Simone <simone.dangelo636@gmail.com>
2024-01-18 12:12:30 +0100

    again

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -7,14 +7,14 @@ const TopNavBar = () => {
       expand="lg"
       className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="https://gioLaghetto.netlify.app/">Giovanni Laghetto</Navbar.Brand>
+        <Navbar.Brand href="https://giolaghetto.netlify.app/">Giovanni Laghetto</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="https://gioLaghetto.netlify.app/">Home</Nav.Link>
-            <Nav.Link href="https://gioLaghetto.netlify.app/iam">Who I am</Nav.Link>
-            <Nav.Link href="https://gioLaghetto.netlify.app/somework">Some Work</Nav.Link>
-            <Nav.Link href="https://gioLaghetto.netlify.app/contact">Contact</Nav.Link>
+            <Nav.Link href="https://giolaghetto.netlify.app/">Home</Nav.Link>
+            <Nav.Link href="https://giolaghetto.netlify.app/iam">Who I am</Nav.Link>
+            <Nav.Link href="https://giolaghetto.netlify.app/somework">Some Work</Nav.Link>
+            <Nav.Link href="https://giolaghetto.netlify.app/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
